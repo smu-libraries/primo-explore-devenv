0a80c8ee674636d9c49d4fb5bdc0d149281eb14d
Ler Wee Hiong <hum.ler@outlook.com>
2016-11-29 14:15:17 +0800

    Set SMU's proxy server

--- gulp/config.js
+++ gulp/config.js
@@ -69,7 +69,7 @@ var SERVERS = {
 };
 
 /*Note that for SSL environments (https) define the server as: var PROXY_SERVER = https://your-server:443*/
-var PROXY_SERVER = 'http://your-server:your-port';
+var PROXY_SERVER = 'https://primo-smu-sb.hosted.exlibrisgroup.com:443';
 
 
 
